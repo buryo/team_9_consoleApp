703f3b6049934dcb9a20f53c3ac05cacc184f5d4
Navid Haghighi <navidhaghighi.1900@gmail.com>
2019-02-05 17:58:58 +0100

    Guessing Number class

--- ConsoleApp1/GuessGame.cs
+++ ConsoleApp1/GuessGame.cs
@@ -9,27 +9,64 @@ namespace ConsoleApp1
             Console.WriteLine("Player 1 what is your name?");
             string player1 = Console.ReadLine();
 
+            Console.WriteLine("--------------------------------------------------------------");
+
             Console.WriteLine("Player 2 what is your name?");
             string player2 = Console.ReadLine();
 
+            Console.WriteLine("--------------------------------------------------------------");
+
+
             Console.WriteLine(player1 + " vs " + player2);
 
-            Console.WriteLine("Let the game begin! Press any key to continue");
+
+            Console.WriteLine("Let the game begin!");
 
             Console.WriteLine("Guess the number! hint: between 0 and 1000");
-            Console.WriteLine(player1 + " :");
+
+            Console.WriteLine("--------------------------------------------------------------");
 
 
 
+            //this variable generates a random number between 1 and 999 (minValue is included, maxValue is not included)
             Random random = new Random();
             int winNumber = random.Next(1, 1000);
 
+            //variable win is false till player wins. 
             bool win = false;
 
+            int playerTurn = 1;
+
+            //do loop which loops till number is guessed right. 
             do
             {
-                Console.Write("Guess the number " + player1 + " :");
+                
+                if (playerTurn == 1)
+                {
+                    Console.Write("Guess the number " + player1 + ": ");
+
+                    Console.WriteLine("--------------------------------------------------------------");
+
+                }
+                else
+                {
+                    Console.Write("Guess the number " + player2 + ": ");
+
+                    Console.WriteLine("--------------------------------------------------------------");
+
+                }
                 string answer = Console.ReadLine();
+
+                //This turns the turn between 1 and 2
+                if (playerTurn == 1)
+                {
+                    playerTurn = 2;
+                }
+                else
+                {
+                    playerTurn = 1;
+                }
+
 
                 int i = int.Parse(answer);
                 if (i > winNumber)
